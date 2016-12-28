5b81fa59b5ce3ef18f852666e0ab1716ed8cc623
mgdickson <matthew.g.dickson@gmail.com>
2016-12-28 17:54:02 +1100

    test

---
+++ code/exercise_16_1.py
@@ -0,0 +1,33 @@
+"""
+
+File: exercise_16_1
+Created: 28/12/2016
+
+
+Copyright 2016 MDickson
+
+"""
+
+from Time1 import *
+
+
+def mul_time(time,mult):
+    """
+
+    :param (Time) time:
+    :param (int) mult:
+    :return: (Time)
+    """
+    return int_to_time(mult * time_to_int(time))
+
+
+
+def main():
+    """
+    The main body of the script
+
+    """
+    # test_time = Time()
+
+if __name__ == '__main__':
+    main()
